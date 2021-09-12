11b971c7c9c8c27a1f3f7eb475995a2cbe550c62
tankalxat34 <89201004+tankalxat34@users.noreply.github.com>
2021-09-12 18:04:32 +0300

    New version: 1.4
    VERSION CHANGES:
    1. Added this exceptions: UploadgramConnectionError, UploadgramUsingKeyError and UploadgramFileIsNotAvalible;
    1.1. UploadgramConnectionError: if uploadgram.me will fall down, you will get this error
    1.2. UploadgramUsingKeyError: if you set key parameter equal "none" string and you will try to rename or delete the file, you will get this error
    1.3. UploadgramFileIsNotAvalible: if you trying to connection to file that does not exists, you will get this error
    
    2. Added example of use into __doc__;
    
    3. Some other edits;

--- UploadgramPyAPI.py
+++ UploadgramPyAPI.py
@@ -4,7 +4,6 @@
 *--------UploadgramPyAPI-------*
 *------------------------------*
 ********************************
-
 Author: tankalxat34
 Description: 
     This API can be upload, download, remove and rename any files from the service uploadgram.me. Using programming language: Python.
@@ -13,14 +12,58 @@ Contacts (API Author):
     - email: tankalxat34@gmail.com
 Contacts (Uploadgram Author):
     - telegrams channel: https://t.me/uploadgramme
+
+****************************************************************
+*----------------------Example of use:-------------------------*
+****************************************************************
+
+import UploadgramPyAPI
+
+# upload any file
+up_new_file = UploadgramPyAPI.NewFile("D:\\image.jpg")
+up_new_file.upload()
+
+# set up connection to file
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+
+# Download any file
+up_file.download()
+
+# Rename any file
+up_file.rename("ItsNewNameForFile.jpg")
+
+# Delete any file
+up_file.delete()
+
+****************************************************************
 """
 
 import requests, getpass, os, os.path, json
 
-global USER_AGENT
 # You can replace this user-agent any different
+global USER_AGENT
 USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.2"
 
+# If you don't want to rename or delete the file you can use this const for param "key" in class "File"
+global NONE
+NONE = "none"
+
+class UploadgramConnectionError(Exception):
+    def __init__(self):
+        self.message = "uploadgram.me currently unavailable. Please, try again later."
+        super().__init__(self.message)
+
+class UploadgramUsingKeyError(Exception):
+    def __init__(self, text):
+        self.text = text
+        self.message = 'You can not ' + text + ' this file because you using the NONE-const for "key" parameter'
+        super().__init__(self.message)
+
+class UploadgramFileIsNotAvalible(Exception):
+    def __init__(self, id):
+        self.id = id
+        self.message = "This file " + id + " does not exists"
+        super().__init__(self.message)
 
 class File:
     def __init__(self, id: str, key: str):
@@ -34,15 +77,19 @@ class File:
         self.url = "https://dl.uploadgram.me/" + self.id
         self.url_import = None
 
-        self.r = requests.get("https://api.uploadgram.me/get/" + self.id)
+        try:
+            self.r = requests.get("https://api.uploadgram.me/get/" + self.id)
+        except requests.exceptions.ConnectionError:
+            raise UploadgramConnectionError()
+
         if self.r:
-            self.file = self.r.json()
+            self.json = self.r.json()
 
             ### Create attibutes for class ###
-            self.name = self.file["filename"]
-            self.size = self.file["size"]
-            self.userTelegramId = self.file["userTelegramId"]
-            self.userIp = self.file["userIp"]
+            self.name = self.json["filename"]
+            self.size = self.json["size"]
+            self.userTelegramId = self.json["userTelegramId"]
+            self.userIp = self.json["userIp"]
 
             ### Create import link ###
             dict_part_url_to_import = ({str(self.key): {'filename': os.path.basename(self.name),
@@ -50,13 +97,13 @@ class File:
                                                            'url': str(self.url)}})
             self.url_import = "https://uploadgram.me/upload/#import:" + json.dumps(dict_part_url_to_import)
         else:
-            raise ValueError(f"File with this id ({self.id}) does not exist")
+            raise UploadgramFileIsNotAvalible(self.id)
 
     def get_json(self):
         """
         Returns json response from answer
         """
-        return self.file
+        return self.json
 
     def download(self, path=f"C:\\Users\\{getpass.getuser()}\\Downloads\\"):
         """
@@ -70,17 +117,23 @@ class File:
         """
         Returns response from server if file deleted successfully
         """
-        self.r_delete = requests.get("https://api.uploadgram.me/delete/" + self.key,
+        if self.key != NONE:
+            self.r_delete = requests.get("https://api.uploadgram.me/delete/" + self.key,
                                      headers={"user-agent": USER_AGENT})
+        else:
+            raise UploadgramUsingKeyError("delete")
         return self.r_delete
 
     def rename(self, new_name: str):
         """
         Returns response from server if file renamed successfully
         """
-        self.r_rename = requests.post("https://api.uploadgram.me/rename/" + self.key,
+        if self.key != NONE:
+            self.r_rename = requests.post("https://api.uploadgram.me/rename/" + self.key,
                                       data={"new_filename": new_name,
                                             "user-agent": USER_AGENT})
+        else:
+            raise UploadgramUsingKeyError("rename")
         return self.r_rename
 
 
@@ -89,20 +142,25 @@ class NewFile:
         """
         :param path:    Path to file what you want to upload
         """
-        self.path = path
-        self.url_upload = 'https://api.uploadgram.me/upload'
-        self.file = open(self.path, "rb")
 
-        self.key = None
-        self.id = None
-        self.url = None
-        self.url_import = None
+        try:
+            self.check_to_avaliable = requests.get("https://api.uploadgram.me/status", headers={"user-agent": USER_AGENT})
+            self.path = path
+            self.url_upload = 'https://api.uploadgram.me/upload'
+            self.json = open(self.path, "rb")
+
+            self.key = None
+            self.id = None
+            self.url = None
+            self.url_import = None
+        except Exception:
+            raise UploadgramConnectionError()
 
     def upload(self):
         """return {'ok': True, 'url': 'https://dl.uploadgram.me/new_file_id', 'delete': 'delete_key'}"""
         self.r = requests.post(self.url_upload,
                                headers={"user-agent": USER_AGENT},
-                               files={"file_upload": self.file})
+                               files={"file_upload": self.json})
         self.key = self.r.json()["delete"]
         self.id = self.r.json()["url"].split("/")[-1]
         self.url = self.r.json()["url"]
